79e94b13a26987eefb40c94e65b0bf806379a1f4
Brice <bricel67@gmail.com>
2022-08-06 18:23:31 +0200

    Style the story section and install tailwind-scrollbar

--- components/Stories.js
+++ components/Stories.js
@@ -19,7 +19,7 @@ const Stories = () => {
   }, []);
 
   return (
-    <div>
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
       {storyUser.map((user) => (
         <Story key={user.id} username={user.username} img={user.img} />
       ))}
