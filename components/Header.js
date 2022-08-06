103e3ff46a3786e08e3c52ae3f8f48630a47757a
Brice <bricel67@gmail.com>
2022-08-06 17:28:13 +0200

    Add the menu and profile image at the right side of the header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/outline";
+import { PlusCircleIcon, SearchIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
+    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
       {/* Left */}
       <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
         <Image
@@ -38,7 +39,15 @@ const Header = () => {
         />
       </div>
       {/* Right */}
-      <h1>Right side</h1>
+      <div className="flex space-x-4 items-center">
+        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3oqKdt668v2xfxVct01uxlOVl8CPHIqHJ-EDplCa3oA&s"
+          alt="avatar"
+          className="h-10 rounded-full cursor-pointer"
+        />
+      </div>
     </div>
   );
 };
